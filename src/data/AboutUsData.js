4461c4987c8b0ef4570159709164456f42102121
deppravacion <deppravacion@gmail.com>
2022-10-12 17:18:30 -0600

    project reset

--- src/data/AboutUsData.js
+++
@@ -1,8 +0,0 @@
-export const aboutUsData = [
-	{
-		title: 'Basic',
-		price: '$7.99 user/month after offer period',
-		numAcc: '50-100 accounts',
-		features: ['Secure accounts', 'Best for small business', 'Easy', 'Affordable', '1GB RAM'],
-	},
-];
--- src/pages/AboutUsPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AboutUs from '../components/AboutUs/AboutUs.js';
-
-const AboutUsPage = () => {
-	return <AboutUs />;
-};
-
-export default AboutUsPage;
